b97fd867f57c7d394286c522153636cd5efe8fa3
sergopavlov <sergopavlov000@gmail.com>
2024-05-28 00:09:14 +0700

    add pointGeneration

--- SKT/Program.cs
+++ SKT/Program.cs
@@ -38,3 +38,17 @@ var asd = revSolvwer.Minimize(new LeastSquaresFunctional(data, mesh.Elements.Cou
 
 
 Console.WriteLine("Hello, World!");
+
+List<Vector3D> GeneratePoints(double x0,double x1,int xn,double y0 ,double y1,int yn,double z)
+{
+    List<Vector3D> points = [];
+    for (int i = 0; i < xn; i++)
+    {
+        for (int j = 0; j < yn; j++)
+        {
+            points.Add(new Vector3D(x0 + i * (x1 - x0) / (xn - 1), y0 + j * (y1 - y0) / (yn - 1), z));
+        }
+    }
+    return points;
+}
+
