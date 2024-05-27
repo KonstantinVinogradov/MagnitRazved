ab9d4e8d0ced7574b466237f0e4911997f1edd75
sergopavlov <sergopavlov000@gmail.com>
2024-05-27 23:16:01 +0700

    everything working 2

---
+++ GUI/MeshSaveModel.cs
@@ -0,0 +1,16 @@
+﻿using SKT.Interfaces;
+using SKT.Mesh;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI;
+public class MeshSaveModel
+{
+
+
+    public Mesh Mesh { get; set; }
+    public List<Material> Materials { get; set; }
+}
